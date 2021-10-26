c92d2f515cebe771d6d0b8a59b0dc0df0642050d
Chinsan Chung <chinsanchung@gmail.com>
2021-10-26 15:09:06 +0900

    console.log 주석 처리. 응답 메시지 수정

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -4,15 +4,15 @@ export default class AuthController {
   constructor() {}
 
   login(req, res, next) {
-    console.log('start login');
+    // console.log('start login');
     passport.authenticate('local', (authError, user, info) => {
       if (authError) {
-        console.log('Login Error:', authError);
+        // console.log('Login Error:', authError);
         res.clearCookie('connect.sid');
         return res.status(403).send('로그인 인증에 에러가 발생했습니다.');
       }
       if (!user) {
-        console.log('No User: ', info);
+        // console.log('No User: ', info);
         res.clearCookie('connect.sid');
         return res.status(403).send(info.message);
       }
@@ -21,8 +21,8 @@ export default class AuthController {
           res.clearCookie('connect.sid');
           return next(loginError);
         } else {
-          console.log('Login Success');
-          return res.send(`Login Success with ID: ${user?.id}`);
+          // console.log('Login Success');
+          return res.send('로그인 완료');
         }
       });
     })(req, res, next);
@@ -33,7 +33,7 @@ export default class AuthController {
     req.session.destroy(() => {
       console.log('Start Logout');
       res.clearCookie('connect.sid');
-      return res.send('Logout Success');
+      return res.send('로그아웃을 완료했습니다.');
     });
   }
 }
