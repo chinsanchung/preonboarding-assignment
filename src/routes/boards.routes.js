bb5b111492980ca822098b53d77dd0b71cf368e8
Chinsan Chung <chinsanchung@gmail.com>
2021-10-27 13:33:34 +0900

    글 작성, 수정, 삭제 전에 로그인 여부 체크

--- src/routes/boards.routes.js
+++ src/routes/boards.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import BoardController from '../api/boards/boards.controller';
+import { isLoggedIn } from '../middlewares/authority';
 
 const router = Router();
 const controller = new BoardController();
 
 router.get('/', controller.getMany);
 router.get('/:board_id', controller.getOne);
-router.post('/', controller.create);
-router.patch('/', controller.update);
-router.delete('/:board_id', controller.delete);
+router.post('/', isLoggedIn, controller.create);
+router.patch('/:board_id', isLoggedIn, controller.update);
+router.delete('/:board_id', isLoggedIn, controller.delete);
 
 export default router;
